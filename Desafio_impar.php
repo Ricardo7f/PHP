e52609ab9bc29bdbd2c51673a0265c3c9eee803b
Ricardo7f <103971703+Ricardo7f@users.noreply.github.com>
2022-05-15 12:04:30 -0300

    Add files via upload

---
+++ Desafio_impar.php
@@ -0,0 +1,9 @@
+<?php
+
+for ($contador = 1; $contador<100; $contador++){
+        if($contador % 2 !=0){
+        echo "<br/>" ,$contador;
+}}
+
+#desafio numeros impares.
+#pegar todos os numeros impares de 1 a 100.
