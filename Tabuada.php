760a37bf1de9060f13cb44441971b3e4934d78c1
Ricardo7f <103971703+Ricardo7f@users.noreply.github.com>
2022-05-15 13:19:49 -0300

    Add files via upload

---
+++ Tabuada.php
@@ -0,0 +1,9 @@
+<?php
+$numero = 3;
+
+for ($contador = 1; $contador <= 10; $contador++) {
+
+    $tabuada = $contador * $numero;
+    echo "3 x $contador=   " . $tabuada . "<br/>";
+    echo "<br/>";
+}
